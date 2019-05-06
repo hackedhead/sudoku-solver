307f5ce2f1e9edc346aaaca62a3b3f40d077f858
Andrew Lindberg <alindberg@fastmail.net>
2019-05-06 14:39:28 -0400

    Fix solver test

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -4,9 +4,10 @@ from sudoku import Solver
 def test_set_cell_value_clears_option_from_row_cells():
     solver = Solver()
     solver.set((1, 6), 5)
-    for cell in [solver.board[1][i] for i in range(0, 9)]:
+    for i in range(0, 9):
+        cell = solver.board[1][i]
         if i != 6:
             assert 5 not in cell.options
         else:
-            assert cell.value == 6
+            assert cell.value == 5
             assert cell.options == set()
