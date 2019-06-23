f156ce671ee00448801ad89b1a9adaac7d9927d9
Andrew Lindberg <alindberg@fastmail.net>
2019-06-22 22:40:45 -0400

    Move smart set function to Board class from Solver
    The Board is responsible for maintaining a consistent board state. The
    function to set a Cell's value also updates the related rows, columns
    and 3x3 squares to remove the set value from other cell's option sets.

--- sudoku.py
+++ sudoku.py
@@ -1,14 +1,11 @@
 from __future__ import print_function
-import sys
-import os
-import itertools
 import re
 from collections import Counter
 
 class Cell:
   def __init__(self, value=None):
     self.value = value
-    self.options = set() if value else set(range(1,10))
+    self.options = set() if value else set(range(1, 10))
 
   def remove_option(self, opt_value):
       self.options.discard(opt_value)
@@ -33,30 +30,39 @@ class Solver:
     def _build_board(self):
         self.board = Board()
 
-    def set(self, cell, value):
-        x, y = cell
-        self.board[x][y].set_value(value)
-        # remove option for other cells in the row and column
-        for i in range(0,9):
-            if i != x:
-                self.board[i][y].remove_option(value)
-            if i != y:
-                self.board[x][i].remove_option(value)
 
 class Board:
-    def __init__(self, board_string = None):
-        self.data = {k:{j:Cell() for j in range(0,9)} for k in range(0,9)}
+    def __init__(self, board_string=None):
+        self.data = {k: {j: Cell() for j in range(0, 9)} for k in range(0, 9)}
         if board_string is not None:
             pattern = re.compile("[1-9X]{81}")
             if not pattern.fullmatch(board_string):
                 raise ValueError
             else:
-                for x in range(0,9):
+                for x in range(0, 9):
                     row_offset = x * 9
                     row_string = board_string[row_offset:row_offset+9]
                     for y, char in enumerate(row_string):
                         if char is not "X":
-                            self.data[x][y].set_value(int(char))
+                            self.set((x, y), int(char))
+
+    def set(self, cell, value):
+        x, y = cell
+        print("attempting to assign {value} to ({x},{y})".format(value=value, x=x, y=y))
+        if value not in self.data[x][y].options:
+            raise ValueError("{value} is not a valid option for Cell({x},{y})".format(x=x, y=y, value=value))
+        print("options for ({x},{y}): {options}".format(x=x, y=y, options=self.data[x][y].options))
+        self.data[x][y].set_value(value)
+        # remove option for other cells in the row and column
+        for i in range(0, 9):
+              self.data[i][y].remove_option(value)
+              self.data[x][i].remove_option(value)
+        # remove option for other cells in square (3x3)
+        xbase = x - (x % 3)
+        ybase = y - (y % 3)
+        for sq_x in range(xbase, xbase+3):
+          for sq_y in range(ybase, ybase+3):
+            self.data[sq_x][sq_y].remove_option(value)
 
 
 
@@ -65,5 +71,3 @@ class Board:
 
     def __setitem__(self, key, value):
         self.data[key] = value
-
-
--- tests/test_board.py
+++ tests/test_board.py
@@ -33,3 +33,21 @@ def test_valid_string_becomes_board():
     assert board[0][8].value == 9
     assert board[1][5].value == None
 
+def test_impossible_columns_string_errors():
+    with pytest.raises(ValueError):
+        assert Board("123456789" * 9)
+
+def test_impossible_squares_string_errors():
+    with pytest.raises(ValueError):
+        assert Board("123456789" + "231564897" + ("X" * 9) * 7)
+
+def test_set_cell_value_clears_option_from_row_cells():
+    board = Board()
+    board.set((1, 6), 5)
+    for i in range(0, 9):
+        cell = board[1][i]
+        if i != 6:
+            assert 5 not in cell.options
+        else:
+            assert cell.value == 5
+            assert cell.options == set()
